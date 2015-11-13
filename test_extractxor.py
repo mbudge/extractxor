cac607919786ff04bfd8ba2468cba18cbed78183
k287 <mbudge1@gmail.com>
2015-11-13 14:13:45 +0000

    Update test_extractxor.py

--- test_extractxor.py
+++ test_extractxor.py
@@ -3,9 +3,9 @@ from operator import xor
 # Set the xor_key
 # Set the image and executable filename or filepath
 #The script xor's the executable and embeds it at the end of the file.
-XOR_KEY = 16
-IMAGE_FILE = 'surfing.jpg'
-EXECUTABLE_FILE = 'winiso.exe'
+XOR_KEY = 1
+IMAGE_FILE = 'FILENAME.jpg'
+EXECUTABLE_FILE = 'FILENAME.exe'
 
 image = open(IMAGE_FILE, 'rb').read()
 
